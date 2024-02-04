de2a13b05e57719422c31b958071324219af1021
DmitryLyakhivsky <dlyakhivskiy@gmail.com>
2024-02-04 23:06:22 +0200

    delete return

--- js/task-4.js
+++ js/task-4.js
@@ -4,21 +4,17 @@ function getShippingCost(country) {
 
   switch (country) {
     case "China": price = 100;
-      message = `Shipping to ${country} will cost ${price} credits`;
       break;
     case "Chile": price = 250;
-      message = `Shipping to ${country} will cost ${price} credits`;
       break;
     case "Australia": price = 170;
-      message = `Shipping to ${country} will cost ${price} credits`;
       break;
     case "Jamaica": price = 120;
-      message = `Shipping to ${country} will cost ${price} credits`;
       break;
     default: message = "Sorry, there is no delivery to your country";
             break;
   }
-  return message;
+  return message = `Shipping to ${country} will cost ${price} credits`;;
 }
 
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
